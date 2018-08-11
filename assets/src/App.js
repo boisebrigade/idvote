0648013be03b2c2ca67fbec0187d3c414c29d2ab
benkline <ben.kline@gmail.com>
2018-08-11 13:20:10 -0600

    add Form, add bootstrap

--- assets/src/App.js
+++ assets/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+
+import Form from './Form';
 
 class App extends Component {
   render() {
@@ -8,11 +10,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to IDVotes!</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Enter your address to find where you vote.
         </p>
+        <Form />
       </div>
     );
   }
--- assets/src/index.js
+++ assets/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './bootstrap.min.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
