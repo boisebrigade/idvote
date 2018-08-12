3e7ad777bbdd2d034641cb29e9b6119023f5bc82
Tyler Samples <samples.tm@gmail.com>
2018-08-11 22:20:04 -0600

    Removing seeders for the time being

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,11 +9,3 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
-
-if Mix.env == :dev do
-  alias Ecto.Adapters.SQL
-
-  querystring = File.read!("precincts.sql" |> Path.expand(__DIR__))
-
-  SQL.query(Idvote.Repo, querystring , [])
-end
