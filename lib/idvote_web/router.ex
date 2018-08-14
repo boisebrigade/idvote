2b7f8b45ae308a575b1fa9d16d78dacf7f1991e7
Tyler Samples <samples.tm@gmail.com>
2018-08-14 00:25:24 -0600

    Forward all non-api and css/js requests to index.html

--- lib/idvote_web/router.ex
+++ lib/idvote_web/router.ex
@@ -5,10 +5,18 @@ defmodule IdvoteWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :browser do
+    plug :accepts, ["html"]
+  end
+
   scope "/api", IdvoteWeb do
     pipe_through :api
 
     post("/find", WebController, :find)
     post("/sms", TwilioController, :sms)
+  end
+
+  scope "/", IdvoteWeb do
+    forward "/", StaticPlug
   end
 end
