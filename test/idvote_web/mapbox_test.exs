8e0912d3dfe8250251cbd944f4604237b528ccc7
Tyler Samples <samples.tm@gmail.com>
2018-08-25 14:00:34 -0600

    Adding precinct resolver test and refactoring both mapbox and precinct tests

--- test/idvote_web/mapbox_test.exs
+++ test/idvote_web/mapbox_test.exs
@@ -18,10 +18,10 @@ defmodule IdvoteWeb.MapboxTest do
   end
 
   test "our mapbox Geoencode has the correct encoding and query parameters" do
-    request = Idvote.Mapbox.Geoencode.encode(@dummy_address)
+    request = Idvote.Mapbox.geoencode(@dummy_address)
 
     assert {:ok, _} = request
-    assert {:ok,  %Tesla.Env{query: %{access_token: _, country: _, limit: _, types: _}}} = request
+    assert {:ok,  %Tesla.Env{query: %{access_token: _, bbox: _, country: _, limit: _, proximity: _, types: _}}} = request
     assert {:ok, %Tesla.Env{url: @mock_url}} = request
   end
 end
